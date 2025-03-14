6b0d6b8532b0b24d3e764deddfd60ca1ccb0b689
saad589 <39567015+saad589@users.noreply.github.com>
2025-03-14 21:33:24 +0600

    Add files via upload

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'MATXCOM v0.3-alpha - Initial Pre-release',
+    VERSION: 'MATXCOM v1.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
